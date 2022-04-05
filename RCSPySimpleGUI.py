11e9cdbdc40f40ae96853f9a0f4862ca675fa93f
AntonioLansang <antoniolansang500@gmail.com>
2022-04-05 17:10:53 -0500

    Rearranged the functions 5-April-2022 17:10pm

--- RCSPySimpleGUI.py
+++ RCSPySimpleGUI.py
@@ -3,44 +3,52 @@ import tkinter
 import PySimpleGUI as sg
 
 
-layout = [
-        [sg.Text("Test")],
-        [sg.Button("Search")]
 
+#Reddit set up
+def Search():
+    reddit=praw.Reddit(
+
+            client_id='sO1ypuzMA0DRwgD8aLcHCQ',
+            client_secret='AAfpbdUosIlYfaxSjWuYzPDJ8rzqQA',
+            username='',
+            password='',
+            user_agent='RedditCommentSearch'
+
+            )
+
+    RedditorToSearch=input(" Input The Redditor")
+    Term=input("Input the Phrase: ")
+
+
+    for comment in reddit.redditor(RedditorToSearch).comments.new(limit=None):
+        CommentContents=comment.body
+       
+       # print(CommentContents)
+        if Term in CommentContents: 
+            print(comment.submission.title)
+            print(CommentContents)
+
+
+sg.theme('DarktanBlue')
+
+layout = [
+        [sg.Text("Welcome")],
+        [sg.Button("Search")],
+        [sg.Button("Exit")],
+        
 ]
 
 
-window=sg.Window("Reddit Comment Search", layout).Finalize()
-window.Maximize()
+window=sg.Window("Reddit Comment Search", layout, size=(800,600), resizable=True).Finalize()
+#window.Maximize()
 
 while True:
     event, values=window.read()
-    if event == "Search" or event == sg.WIN_CLOSED:
+    if event == "Exit" or event == sg.WIN_CLOSED:
         break
+    if event == "Search":
+        Search()
 window.close()
 
 
-
-#Reddit set up
-reddit=praw.Reddit(
-
-        client_id='sO1ypuzMA0DRwgD8aLcHCQ',
-        client_secret='AAfpbdUosIlYfaxSjWuYzPDJ8rzqQA',
-        username='',
-        password='',
-        user_agent='RedditCommentSearch'
-
-        )
-
-RedditorToSearch=input(" Input The Redditor")
-Term=input("Input the Phrase: ")
-
-
-for comment in reddit.redditor(RedditorToSearch).comments.new(limit=None):
-    CommentContents=comment.body
-   
-   # print(CommentContents)
-    if Term in CommentContents: 
-        print(comment.submission.title)
-        print(CommentContents)
 
